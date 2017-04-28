ece6f97a6e007570ff26f544b9e037bf48b392b7
JulaB <julia.bazhuhina@gmail.com>
2017-04-27 21:24:21 -0700

    Kth smallest

--- trees/traversal.rb
+++ trees/traversal.rb
@@ -1,11 +1,29 @@
 require_relative 'binary_tree'
 
+KCounter = Struct.new(:val)
+
 class BinaryTree
   def inorder_traversal
     self.class.inorder_traversal(@root)
   end
 
+  def kth_smallest(k)
+    self.class.kth_smallest(@root, KCounter.new(k))
+  end
+
   private
+
+  def self.kth_smallest(root, k)
+    return if root.nil?
+
+    smallest = kth_smallest(root.left, k) if root.left
+
+    k.val -= 1
+    return root.data if k.val.zero? && !smallest
+
+    smallest = kth_smallest(root.right, k) if root.right && !smallest
+    smallest
+  end
 
   def self.inorder_traversal(root)
     return [] if root.nil?
@@ -28,4 +46,3 @@ class BinaryTree
     res
   end
 end
-
